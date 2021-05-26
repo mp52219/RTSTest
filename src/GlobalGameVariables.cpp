518164490c854152f808d664cbeb6fa65cc8cbfe
Marin <wanderingcake@gmail.com>
2021-05-26 19:43:59 +0200

    Fixed spawning of units

--- src/GlobalGameVariables.cpp
+++ src/GlobalGameVariables.cpp
@@ -8,10 +8,7 @@ const int screenSize = 800;
 sf::RenderWindow screen(sf::VideoMode(screenSize, screenSize), "Dijkstra and A*");
 sf::Event event;
 sf::Color color(sf::Color::Black);
-sf::CircleShape circle(200);
-sf::Clock clk;
 sf::View view1(sf::FloatRect(0.f, 0/*gameSize - 10.f * tileSize*/, 30.f * tileSize, 30.f * tileSize));
-const int MAX = gameSize;
 const int EFS = gameSize / tileSize;
 const int EFS2 = EFS * EFS;
 char map[EFS2];
@@ -19,4 +16,3 @@ char map2[EFS2];
 int slow = 20;
 int isAStar = 0;
 int isVisualization = 0;
-//TODO REMOVE EXCESS
